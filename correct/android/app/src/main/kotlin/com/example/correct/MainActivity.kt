a0d4ca4f5e1f07640a9ea0cb07ad719b50601f8f
Brian Nganga <briannganga70@gmail.com>
2024-04-30 14:30:24 +0300

    working

--- correct/android/app/src/main/kotlin/com/example/correct/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.correct
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/home.dart
+++ lib/home.dart
@@ -10,24 +10,80 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late WebViewController controller;
+  bool isLoading = false; // Flag to track loading state
+  bool connectionError = false; // Flag to track connection error
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CodenJobs App'),
-      ),
-      body: WebView(
-        javascriptMode: JavascriptMode.unrestricted,
-        initialUrl: "https://codenjobs.com",
-        onWebViewCreated: (controller) {
-          this.controller = controller;
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          controller.reload();
-        },
-        child: const Icon(Icons.refresh),
+    return WillPopScope(
+      onWillPop: () async {
+        if (await controller.canGoBack()) {
+          controller.goBack();
+          return false; // Do not exit the app
+        }
+        return true; // Exit the app
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebView(
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: "https://magicalkenya.com/",
+              onPageStarted: (url) {
+                setState(() {
+                  isLoading = false;
+                  connectionError = false;
+                });
+              },
+              onPageFinished: (url) {
+                setState(() {
+                  isLoading = false;
+                });
+              },
+              onWebViewCreated: (controller) {
+                this.controller = controller;
+              },
+              navigationDelegate: (NavigationRequest request) {
+                // Allow loading only if the requested URL is from the same domain
+                if (request.url.startsWith("https://magicalkenya.com/")) {
+                  return NavigationDecision.navigate;
+                } else {
+                  // Open external links in the default browser
+                  // You can customize this behavior as needed
+                  // Launch the URL in the default browser
+                  // launch(request.url);
+                  return NavigationDecision.prevent;
+                }
+              },
+            ),
+
+            // Show error page when connection error occurs
+            Visibility(
+              visible: connectionError,
+              child: Center(
+                child: Text(
+                  "Connection error! Check your internet connection and try again.",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+
+            // Show loading indicator while content is loading
+            Visibility(
+              visible: isLoading,
+              child: const Center(child: CircularProgressIndicator()),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            setState(() {
+              isLoading = false;
+            });
+            controller.reload();
+          },
+          child: const Icon(Icons.refresh),
+        ),
       ),
     );
   }
